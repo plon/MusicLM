f4584b6e125fc2547473fe6cf25c41673309360f
Evan Chiao <evanc314@gmail.com>
2023-05-23 00:08:14 -0400

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ from MusicLM import Music
 
 # Create a new instance of Music
 music = Music()
+
 input = "Ambient, soft sounding music I can study to"
 tracks = music.get_tracks(input, 2)
 music.base64toMP3(tracks, input)
