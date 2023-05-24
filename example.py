8c6497e625dbb45afc10c14027ff5a0777e5bb6f
Evan Chiao <evanc314@gmail.com>
2023-05-24 15:15:12 -0400

    Update example.py

--- example.py
+++ example.py
@@ -5,4 +5,5 @@ music = Music()
 
 input = "Ambient, soft sounding music I can study to"
 tracks = music.get_tracks(input, 2)
-music.base64toMP3(tracks, input)
+if tracks != "Oops, can't generate audio for that.":
+    music.base64toMP3(tracks, input)
