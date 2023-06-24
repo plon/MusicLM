7b49ab55ba5cadd686889ae3d3d7ff4aa93a6bd7
armintum <115377622+armintum@users.noreply.github.com>
2023-06-24 18:14:33 -0400

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from MusicLM import Music
 # Create a new instance of Music
 music = Music()
 
-input = "Taylor Swift"
+input = "Ambient, soft sounding music I can study to"
 tracks = music.get_tracks(input, 2)
 if not (tracks == "Oops, can't generate audio for that." or tracks == "Can't connect to the server."):
     music.base64toMP3(tracks, input)
